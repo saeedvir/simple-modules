6c345041d1c645d0d8f5c0fda8da335ff905f39d
saeed abdollahian <saeedvir@users.noreply.github.com>
2026-08-07 04:37:54 +0330

    Create modules.php

---
+++ config/modules.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Disabled Modules
+    |--------------------------------------------------------------------------
+    |
+    | List module names (PascalCase) to disable them. Disabled modules are
+    | not loaded, their routes/migrations/config/views are all skipped.
+    |
+    */
+
+    'disabled' => [
+    ],
+];
